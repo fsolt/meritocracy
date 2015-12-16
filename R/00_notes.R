1160a935652570af311e79f90b968695d08e8eea
Frederick Solt <fsolt@mac.com>
2015-12-16 16:16:49 -0600

    calculate level 2 variables by CZ

--- R/00_notes.R
+++ R/00_notes.R
@@ -5,7 +5,20 @@ library(dplyr)
 library(magrittr)
 library(lme4)
 
-# Level 1 Data: Pew 2007 Religious Landscape Survey
+# Commuting Zones
+if (!file.exists("data/cw_cty00_cz.csv")) {
+  temp <- tempfile(fileext = ".zip")
+  download.file("http://www.equality-of-opportunity.org/images/crosswalks.zip", temp)
+  cw <- read_dta(unzip(temp, "cw_cty00_cz.dta"))
+  unlink(temp)
+  write_csv(cw, "data/cw_cty00_cz.csv")
+}
+
+cz <- read_csv("data/cw_cty00_cz.csv") %>% 
+  select(fips = county_id,
+         cz = cz)
+
+### Individual-Level Data: Pew 2007 Religious Landscape Survey
 p2007 <- read_sav("data/dataset_Religious_Landscape_Survey_Data/Religious Landscape Survey Data - Continental US.sav")
 p2007fips <- read_sav("data/dataset_Religious_Landscape_Survey_Data/FIPS Continental US.sav")
 
@@ -28,27 +41,15 @@ p2007x$partyid <- plyr::mapvalues(p2007$party,
 p2007x$partyid[p2007$partyln==1] <- 4
 p2007x$partyid[p2007$partyln==2] <- 2
 
-# Level 2 data:
+p2007x %<>% left_join(cz, by="fips") 
+
+# 2004 Election Data
 fips_cnty <- read_csv("https://raw.githubusercontent.com/raypereda/fips-county-codes/master/lib/national.txt", 
                       col_types="ccccc") 
 names(fips_cnty) <- tolower(gsub(" ", "_", names(fips_cnty)))
 fips_cnty$fips <- as.numeric(do.call(paste0, c(fips_cnty[, c(2,3)])))
 fips_cnty$county <- tolower(gsub(" County| Parish", "", fips_cnty$county_name))
 fips_cnty$county <- gsub(" ", "", fips_cnty$county)
-
-cz <- read_dta("Equality of Opportunity Project/replicate/clean_public_data/crosswalks/county_2000.dta") %>% 
-  select(fips = county_id, cz)
-
-download.file("http://www.equality-of-opportunity.org/images/online_data_tables.xls", "data/eq_of_opp_online_data.xls")
-cz_data <- read_excel("data/eq_of_opp_online_data.xls", 
-                      sheet = "Online Data Table 5", 
-                      skip = 49,
-                      col_types = c("numeric", "text", "text", rep("numeric", 32))) %>% 
-  filter(!is.na(CZ)) %>% 
-  select(cz = CZ,
-         p_c5p1 = `P(Child in Q5 | Parent in Q1), 80-85 Cohort`) %>% 
-  right_join(cz)
-
 
 bush04 <- read_tsv("http://bactra.org/election/vote-counts-with-NE-aggregated")
 bush04$perc_bush04 <- with(bush04, Bush/(Bush+Kerry+Nader))
@@ -77,11 +78,11 @@ bush04$county[(bush04$state=="VT") & bush04$county=="oran"] <- "orange"
 bush04$county[(bush04$state=="VA") & bush04$county=="manassas"] <- "manassascity"
 bush04$county[(bush04$state=="VA") & bush04$county=="norton"] <- "nortoncity"
 
-
-bush04_cnty <- left_join(bush04, fips_cnty)
+bush04_cnty <- left_join(bush04, fips_cnty, by=c("state", "county"))
 missing <- bush04_cnty[is.na(bush04_cnty$fips), 1:8] # election results still without fips due to county name inconsistencies
 bush04_cnty <- bush04_cnty[!is.na(bush04_cnty$fips), ] # keep only results that already have fips
-remaining <- anti_join(fips_cnty, bush04) %>% arrange(state) # fips without election results
+remaining <- anti_join(fips_cnty, bush04, by=c("state", "county")) %>%
+  arrange(state) # fips without election results
 
 missing$county0 <- missing$county # move county names to a tempvar
 missing$county <- NA
@@ -93,11 +94,49 @@ for(i in 1:length(states)) {
   missing$county[missing$state==states[i]] <- lapply(missing$county0[missing$state==states[i]], function (ii) agrep(ii, t.rem, value=T, max.distance=.2)) # find matches to county name by state
 }
 missing$county <- unlist(lapply(missing$county, function(ii) ii[1])) # use closest match to county name
-missing <- left_join(missing, fips_cnty) # now merge; some results still without fips in Maine, otherwise good
+missing <- left_join(missing, fips_cnty, by=c("state", "county")) # now merge; some results still without fips in Maine, otherwise good
 missing$county0 <- NULL # drop tempvar
 
 bush04_cnty %<>% rbind(missing) 
-cz_data2 <- left_join(cz_data, bush04_cnty)
+
+cz_bush04 <- left_join(bush04_cnty, cz, by="fips") %>% 
+  group_by(cz) %>% 
+  summarize(bush04 = sum(bush)/sum(bush+kerry+nader))
+
+# Equality of Opportunity
+if (!file.exists("data/eq_of_opp_online_data.xls")) {
+  download.file("http://www.equality-of-opportunity.org/images/online_data_tables.xls", "data/eq_of_opp_online_data.xls")
+}
+
+cz_eo <- read_excel("data/eq_of_opp_online_data.xls", 
+                      sheet = "Online Data Table 5", 
+                      skip = 49,
+                      col_types = c("numeric", "text", "text", 
+                                    rep("numeric", 32))) %>% 
+  filter(!is.na(CZ)) %>% 
+  select(cz = CZ,
+         am = `AM, 80-82 Cohort`,
+         rm = `RM, 80-82 Cohort`,
+         p_c5p1 = `P(Child in Q5 | Parent in Q1), 80-85 Cohort`) 
+
+cz_other <- read_excel("data/eq_of_opp_online_data.xls", 
+                          sheet = "Online Data Table 8", 
+                          skip = 6,
+                          col_types = c("numeric", "text", "text", 
+                                        rep("numeric", 38))) %>%
+  filter(!CZ==-1) %>% 
+  transmute(cz = CZ,
+         gini_cz = Gini,
+         income_cz = `Household Income per capita`/10000,
+         black_cz = `Frac. Black`,
+         pop_cz = `Census 2000 population`,
+         seg_race_cz = `Racial Segregation`,
+         seg_inc_cz = `Income Segregation`,
+         seg_pov_cz = `Segregation of Poverty (<p25)`,
+         seg_aff_cz = `Segregation of Affluence (>p75)`) 
+
+cz_all <- left_join(cz_eo, cz_bush04, by="cz") %>% 
+  left_join(cz_other, by="cz")
 
 #HERE: will need to merge cz_data2 with the acs data
 
@@ -105,7 +144,7 @@ acs0509 <- read_csv("data/acs0509-counties.csv") # this throws warnings; they ar
 names(acs0509) <- tolower(names(acs0509))
 acs0509 <- mutate(acs0509,
                   fips = as.numeric(gsub("05000US", "", geoid)),
-                  gini_cnty = b19083_001e,
+                  gini_cz = b19083_001e,
                   income_cnty = b19013_001e/10000,
                   black_cnty = b02001_003e/b02001_001e,
                   pop_cnty = b02001_001e/10000)
