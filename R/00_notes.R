90a7c6fb9daf9a9817d6a806001904dd0c6ad8fb
Frederick Solt <fsolt@mac.com>
2015-12-17 11:54:32 -0600

    clean up

--- R/00_notes.R
+++ R/00_notes.R
@@ -37,8 +37,8 @@ p2007x <- merge(p2007, p2007fips) %>%
             attend = 7 - ifelse(q20<=6, q20, NA)) %>%  # 1 to 6
   rename(fips = fips2)
 p2007x$partyid_rep <- plyr::mapvalues(p2007$party, 
-                            from = c(1:5, 9), 
-                            to = c(5, 1, 3, 3, 3, NA))
+                                      from = c(1:5, 9), 
+                                      to = c(5, 1, 3, 3, 3, NA))
 p2007x$partyid_rep[p2007$partyln==1] <- 4
 p2007x$partyid_rep[p2007$partyln==2] <- 2
 
@@ -110,10 +110,10 @@ if (!file.exists("data/eq_of_opp_online_data.xls")) {
 }
 
 cz_eo <- read_excel("data/eq_of_opp_online_data.xls", 
-                      sheet = "Online Data Table 5", 
-                      skip = 49,
-                      col_types = c("numeric", "text", "text", 
-                                    rep("numeric", 32))) %>% 
+                    sheet = "Online Data Table 5", 
+                    skip = 49,
+                    col_types = c("numeric", "text", "text", 
+                                  rep("numeric", 32))) %>% 
   filter(!is.na(CZ)) %>% 
   select(cz = CZ,
          am_cz = `AM, 80-82 Cohort`,
@@ -121,20 +121,20 @@ cz_eo <- read_excel("data/eq_of_opp_online_data.xls",
          p_c5p1_cz = `P(Child in Q5 | Parent in Q1), 80-85 Cohort`) 
 
 cz_other <- read_excel("data/eq_of_opp_online_data.xls", 
-                          sheet = "Online Data Table 8", 
-                          skip = 6,
-                          col_types = c("numeric", "text", "text", 
-                                        rep("numeric", 38))) %>%
+                       sheet = "Online Data Table 8", 
+                       skip = 6,
+                       col_types = c("numeric", "text", "text", 
+                                     rep("numeric", 38))) %>%
   filter(!CZ==-1) %>% 
   transmute(cz = CZ,
-         gini_cz = Gini,
-         income_cz = `Household Income per capita`/10000,
-         black_cz = `Frac. Black`,
-         pop_cz = `Census 2000 population`/100000,
-         seg_race_cz = `Racial Segregation`,
-         seg_inc_cz = `Income Segregation`,
-         seg_pov_cz = `Segregation of Poverty (<p25)`,
-         seg_aff_cz = `Segregation of Affluence (>p75)`) 
+            gini_cz = Gini,
+            income_cz = `Household Income per capita`/10000,
+            black_cz = `Frac. Black`,
+            pop_cz = `Census 2000 population`/100000,
+            seg_race_cz = `Racial Segregation`,
+            seg_inc_cz = `Income Segregation`,
+            seg_pov_cz = `Segregation of Poverty (<p25)`,
+            seg_aff_cz = `Segregation of Affluence (>p75)`) 
 
 cz_all <- left_join(cz_eo, cz_bush04, by="cz") %>% 
   left_join(cz_other, by="cz")
@@ -149,7 +149,7 @@ p2007x1_w <- p2007x1 %>% filter(white==1) %>% select(-white)
 m1_flat <- glm(formula = rej_merit~income+gini_cz+income:gini_cz+
                  educ+age+male+partyid_rep+ideo_con+attend+
                  income_cz+black_cz+bush04_cz+pop_cz,
-                    data=p2007x1_w, family=binomial(link="logit"))
+               data=p2007x1_w, family=binomial(link="logit"))
 
 m1 <- glmer(formula = rej_merit~income+gini_cz+income:gini_cz+
               educ+age+male+partyid_rep+ideo_con+attend+
@@ -198,17 +198,17 @@ ga12 <- read_sav("Pew/Pew Research Global Attitudes Project Spring 2012 Dataset 
 names(ga12) <- tolower(names(ga12))
 
 ga12x <- data.frame(
-    resp = ga12$psraid,
-    cc = as.numeric(ga12$country),
-    rej_merit = ifelse(ga12$q84<=2, ga12$q84-1, NA),
-    #q156    income = ifelse(ga12$income<=9, ga12$income, NA), # 1 to 9
-    #q154    educ = ifelse(ga12$educ<=7, ga12$educ, NA), # 1 to 7
-    age = ifelse(ga12$q142<99, ga12$q142, NA),
-    male = ifelse(ga12$q141==1, 1, 0),
-    #q159    white = ifelse(ga12$race==1 & ga12$hisp!=1, 1, 0),
-    #na    union = ifelse(ga12$labor<=3, 1, ifelse(ga12$labor==4, 0, NA)),
-    #na    ideo = 6 - ifelse(ga12$ideo<=5, ga12$ideo, NA), # 1 to 5
-    attend = 7 - ifelse(ga12$q153<=6, ga12$q153, NA) # 1 to 6
+  resp = ga12$psraid,
+  cc = as.numeric(ga12$country),
+  rej_merit = ifelse(ga12$q84<=2, ga12$q84-1, NA),
+  #q156    income = ifelse(ga12$income<=9, ga12$income, NA), # 1 to 9
+  #q154    educ = ifelse(ga12$educ<=7, ga12$educ, NA), # 1 to 7
+  age = ifelse(ga12$q142<99, ga12$q142, NA),
+  male = ifelse(ga12$q141==1, 1, 0),
+  #q159    white = ifelse(ga12$race==1 & ga12$hisp!=1, 1, 0),
+  #na    union = ifelse(ga12$labor<=3, 1, ifelse(ga12$labor==4, 0, NA)),
+  #na    ideo = 6 - ifelse(ga12$ideo<=5, ga12$ideo, NA), # 1 to 5
+  attend = 7 - ifelse(ga12$q153<=6, ga12$q153, NA) # 1 to 6
 )
 cn <- data.frame(cc = attr(ga12$country, "labels"))
 cn$country <- row.names(cn)
